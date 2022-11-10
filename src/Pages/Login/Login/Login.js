7c2a2ef6b2c2ba8fe46a9bccd0df133303c52fe5
Achintya151 <achintyakumartalukdar@gmail.com>
2022-11-10 16:39:25 +0600

    firebse connected

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogin = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col">
@@ -9,7 +14,7 @@ const Login = () => {
                     <h1 className="text-5xl font-bold">Login now!</h1>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form className="card-body" onSubmit={handleLogin}>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -31,7 +36,7 @@ const Login = () => {
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Login</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
