14311b1296aea434b9e72c7e878914b50a044c5b
Achintya151 <achintyakumartalukdar@gmail.com>
2022-11-10 17:14:52 +0600

    authentication setup added

--- src/Pages/Login/Signup/Signup.js
+++ src/Pages/Login/Signup/Signup.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+
+    const handleSignup = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+    }
+
     return (
-        <div>
-            <h2>Signup page</h2>
+        <div className="hero min-h-screen min-w-full bg-base-200 ">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left py-5">
+                    <h1 className="text-5xl font-bold">Signup now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+                    <form className="card-body" onSubmit={handleSignup}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input name='name' type="text" placeholder="name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input name='photoURL' type="text" placeholder="Photo URL" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name='email' type="text" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name='password' type="password" placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div>
+                            <p className="py-2 text-center"><small>Already have an account? Please <Link to='/login'>Login.</Link></small></p>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary md:px-40">Signup</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
